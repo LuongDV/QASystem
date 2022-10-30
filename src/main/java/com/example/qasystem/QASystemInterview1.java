e2c43a7cb2313ee3a4091d5ff57bc432003611ca
luongdv <luongdv124@gmail.com>
2022-10-30 17:45:40 +0700

    update

--- src/main/java/com/example/qasystem/QASystemInterview1.java
+++ src/main/java/com/example/qasystem/QASystemInterview1.java
@@ -1,3 +1,5 @@
+package com.example.qasystem;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
--- src/main/java/com/example/qasystem/QASystemInterview2.java
+++ src/main/java/com/example/qasystem/QASystemInterview2.java
@@ -1,3 +1,5 @@
+package com.example.qasystem;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.FileSystems;
